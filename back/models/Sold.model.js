a56409a81886f76d00b43987c6257a7130664fc6
VincentMax90 <maxclc@hotmail.com>
2023-12-26 18:18:40 -0300

    seller

--- back/models/Sold.model.js
+++ back/models/Sold.model.js
@@ -5,6 +5,29 @@ class Sold extends Sequelize.Model {}
 
 Sold.init(
   {
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    seller:{
+      type:Sequelize.STRING,
+      allowNull:false
+    },
+    table: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    
+    
   },
   { sequelize: db, modelName: "Sold" }
 );
