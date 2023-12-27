87bd66edc5c70f703d0ddd692e7bc00472ae6354
VincentMax90 <maxclc@hotmail.com>
2023-12-26 23:08:10 -0300

    model messaje

---
+++ back/models/Messaje.model.js
@@ -0,0 +1,41 @@
+const db = require("../config/db");
+const Sequelize = require("sequelize");
+
+class Messaje extends Sequelize.Model {}
+
+Messaje.init(
+  {
+    date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      
+    },  email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      
+    },
+
+    messaje: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    read: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: false,
+    },
+   
+  },
+  { sequelize: db, modelName: "Messaje" }
+);
+
+module.exports = Messaje;
--- back/models/index.js
+++ back/models/index.js
@@ -1,4 +1,5 @@
 const User = require("./User.model");
 const Menu = require("./Menu.model");
-const Sold = require("./Sold.model")
-module.exports = { User, Menu };
+const Sold = require("./Sold.model");
+const Messaje = require("./Messaje.model");
+module.exports = { User, Menu, Sold, Messaje };
